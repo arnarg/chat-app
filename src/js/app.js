f26db1f54c0201093b6d152ecb12ecf3b6554e84
Atli Guðlaugsson <aaatttllliii@gmail.com>
2015-02-12 16:03:47 +0000

    baetti vid angular virkni

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+angular.module("ChatApp", []);
+angular.module("ChatApp").controller("ChatAppController", function($scope){
+	$scope.message = "Hello world";
+});
+
+var atli = "Hae";
+console.log(atli);
