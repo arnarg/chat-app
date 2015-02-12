ed9d2f68ee5c9712b34105eacf283e659c407557
Arnar Gauti Ingason <granra@gmail.com>
2015-02-12 17:25:13 +0000

    Updated shit

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function (grunt) {
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+ 	var taskConfig = {
+		jshint: {
+       			src: ['src/js/*.js'],
+       			gruntfile: ['Gruntfile.js'],
+			options: {
+			}
+		}
+	};
+   	grunt.initConfig(taskConfig);	
+}
