d8d602cb1020ed8e30cd7889b4029d3c6084daa9
Atli Guðlaugsson <aaatttllliii@gmail.com>
2015-02-12 17:37:16 +0000

    baetti vid sma routing virkni

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,26 @@
-angular.module("ChatApp", []);
-angular.module("ChatApp").controller("ChatAppController", function($scope){
-	$scope.message = "Hello world";
+var ChatClient = angular.module("ChatClient", ['ngRoute']);
+
+ChatClient.config(function($routeProvider){
+	$routeProvider
+	.when("/home/index", {
+		templateUrl: "views/home.html",
+		controller: "HomeController"
+	})
+	.when("/home/about", {
+		templateUrl: "views/about.html",
+		controller: "AboutController"
+	})
+	.otherwise({
+		redirectTo: "home/index"
+	})
 });
 
-var atli = "Hae";
-console.log(atli);
+ChatClient.controller("HomeController", 
+function($scope){
+	$scope.message = "Hello from home";
+});
+
+ChatClient.controller("AboutController", 
+function($scope){
+	$scope.message = "Hello from about";
+});
