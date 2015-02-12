c1392d275e17ae967698e2ca67d0750ad1686bef
Þórdís Jóna <thordisj12@ru.is>
2015-02-12 18:25:39 +0000

    Added uglify and concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,34 @@
 module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
- 	var taskConfig = {
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	var taskConfig = {
+		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
-       			src: ['src/js/*.js'],
-       			gruntfile: ['Gruntfile.js'],
+			src: ['src/js/*.js'],
+			gruntfile: ['Gruntfile.js'],
 			options: {
+			}
+		},
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: ['src/js/*.js'],
+				dest: 'src/js/<%= pkg.name %>.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: {
+					'src/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+				}
 			}
 		}
 	};
-   	grunt.initConfig(taskConfig);	
+	grunt.initConfig(taskConfig);
 };
