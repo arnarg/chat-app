89749fd60f1857da080c298def22bda74f0e8f1f
Atli Guðlaugsson <aaatttllliii@gmail.com>
2015-02-17 16:56:21 +0000

    kick virkni og servermessages

--- src/js/controllers/controllers.js
+++ src/js/controllers/controllers.js
@@ -29,16 +29,26 @@ function ($scope, $location, socket){
 
 ChatClient.controller("RoomController",
 function ($scope, $location, $routeParams, $window, socket){
-	$scope.message = "Hello from Room";
 	$scope.currentRoom = $routeParams.room;
 	$scope.currentUser = $routeParams.user;
 	$scope.currentUsers = [];
 	$scope.errorMessage = "";
 	$scope.messageHistory = [];
 	$scope.newMessage = "";
+	$scope.roomTopic = "";
 
 	socket.on("updatechat", function(roomName, messageHistory){
 		$scope.messageHistory = messageHistory;
+		$scope.roomTopic = "Welcome to the room! Please behave :((";
+	});
+
+	socket.on("servermessage", function(join, room, user){
+		if(join){
+			servermsg = user + " just joined in on the fun!";
+			if($scope.currentUser === user){
+				socket.emit("sendmsg", {roomName: room, msg: servermsg});
+			}
+		}
 	});
 
 	socket.on("updateusers", function(roomName, users, ops){
@@ -51,6 +61,12 @@ function ($scope, $location, $routeParams, $window, socket){
 			console.log("success");
 		} else {
 			console.log(":((");
+		}
+	});
+
+	socket.on("kicked", function(room, kickee, kicker){
+		if(kickee === $scope.currentUser){
+			$location.path("/rooms/" + $scope.currentUser);
 		}
 	});
 	
@@ -66,22 +82,23 @@ function ($scope, $location, $routeParams, $window, socket){
 
 	$scope.back = function() {
 		$window.history.back();
-	}
+	};
 
 	$scope.backToRooms = function(){
 		$location.path("/rooms/" + $scope.currentUser);
-	}
+	};
 
 	$scope.kickUser = function(userToKick){
 		console.log(userToKick);
 		socket.emit("kick", {user: userToKick, room: $scope.currentRoom}, function(success){
 			if(success){
-				console.log("kicked successfully");
+				kickMsg = userToKick + " just got kicked!";
+				socket.emit("sendmsg", {roomName: $scope.currentRoom, msg: kickMsg});
 			} else {
 				console.log("kick failed");
 			}
 		});
-	}
+	};
 });
 
 ChatClient.controller("PrivateController",
