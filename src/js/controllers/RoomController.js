097d88fd3056c80eadba7a9524dccac9b4798899
Arnar Gauti Ingason <granra@gmail.com>
2015-02-25 14:35:47 +0000

    You can now refresh and be redirected to login without crashing server

--- src/js/controllers/RoomController.js
+++ src/js/controllers/RoomController.js
@@ -1,161 +1,170 @@
 ChatClient.controller("RoomController",
-["$scope", "$state", "$stateParams", "socket",
-function ($scope, $state, $stateParams, socket){
-	$scope.currentRoom = $stateParams.room;
-	$scope.currentUser = $stateParams.user;
+["$scope", "$state", "$rootScope", "$stateParams", "socket",
+function ($scope, $state, $rootScope, $stateParams, socket){
+	var init = function() {
+		$scope.currentRoom = $stateParams.room;
+		$scope.currentUser = $stateParams.user;
 
-	$scope.message = "Hello from Room";
-	$scope.currentUsers = {};
-	$scope.currentOps = {};
-	$scope.errorMessage = "";
-	$scope.messageHistory = {
-		public: []
-	};
-	$scope.roomTopic = "";
-	$scope.kick = false;
-	var joinmsg = "";
-	var partmsg = "";
+		$scope.message = "Hello from Room";
+		$scope.currentUsers = {};
+		$scope.currentOps = {};
+		$scope.errorMessage = "";
+		$scope.messageHistory = {
+			public: []
+		};
+		$scope.roomTopic = "";
+		$scope.kick = false;
+		var joinmsg = "";
+		var partmsg = "";
 
-	// Tabs
-	$scope.tabData = [
-		{
-			heading: $scope.currentRoom,
-			route: "room.public",
-			active: true,
-			params: {},
-			closable: false
-		}
-	];
+		// Tabs
+		$scope.tabData = [
+			{
+				heading: $scope.currentRoom,
+				route: "room.public",
+				active: true,
+				params: {},
+				closable: false
+			}
+		];
 
-	$scope.go = function(state, params) {
-		$state.go(state, params);
-	};
+		$scope.go = function(state, params) {
+			$state.go(state, params);
+		};
 
-	$scope.closeTab = function(tab) {
-		var index = $scope.tabData.indexOf(tab);
-		if (index > -1) {
-			$scope.messageHistory[tab.heading] = undefined;
-			$scope.tabData.splice(index, 1);
-		}
-	};
+		$scope.closeTab = function(tab) {
+			var index = $scope.tabData.indexOf(tab);
+			if (index > -1) {
+				$scope.messageHistory[tab.heading] = undefined;
+				$scope.tabData.splice(index, 1);
+			}
+		};
 
-	$scope.createPrivateTab = function(user, isActive) {
-		$scope.tabData.push({
-			heading: user,
-			route: "room.private",
-			active: isActive,
-			params: {
-				other: user
-			},
-			closable: true
+		$scope.createPrivateTab = function(user, isActive) {
+			$scope.tabData.push({
+				heading: user,
+				route: "room.private",
+				active: isActive,
+				params: {
+					other: user
+				},
+				closable: true
+			});
+			$scope.messageHistory[user] = [];
+		};
+
+		// Socket event handlers
+		socket.on("updatechat", function(roomName, messageHistory){
+			$scope.messageHistory.public = messageHistory;
+			$("#chatWindow").animate({ scrollTop: $(document).height() }, "fast");
 		});
-		$scope.messageHistory[user] = [];
-	};
 
-	// Socket event handlers
-	socket.on("updatechat", function(roomName, messageHistory){
-		$scope.messageHistory.public = messageHistory;
-		$("#chatWindow").animate({ scrollTop: $(document).height() }, "fast");
-	});
-
-	socket.on("servermessage", function(status, room, user){
-		if(status === "join"){
-			if($scope.currentUser === user){
-				if(joinmsg === ""){
-					joinmsg = " just joined in on the fun!";
-					socket.emit("sendmsg", {roomName: room, msg: joinmsg});
+		socket.on("servermessage", function(status, room, user){
+			if(status === "join"){
+				if($scope.currentUser === user){
+					if(joinmsg === ""){
+						joinmsg = " just joined in on the fun!";
+						socket.emit("sendmsg", {roomName: room, msg: joinmsg});
+					}
 				}
 			}
-		}
-		else if(status === "quit"){
-			console.log("quit");
-		}
-	});
-
-	socket.on("updateusers", function(roomName, users, ops){
-		if (roomName === $scope.currentRoom) {
-			$scope.currentUsers = users;
-			$scope.currentOps = ops;
-		}
-	});
-
-	socket.on("kicked", function(room, kickee, kicker){
-		if(kickee === $scope.currentUser){
-			$state.go("rooms", { user: $scope.currentUser });
-			toastr.error("Check yo self befo' yo wreck yo self!", "You've been kicked");
-		}
-	});
-
-	socket.on("banned", function(room, bannee, banner){
-		if(bannee === $scope.currentUser){
-			$state.go("rooms", { user: $scope.currentUser });
-			toastr.error("I guess you did not check yourself.", "You have been banned");
-		}
-	});
-
-	socket.on("recv_privatemsg", function(nick, message){
-		if ($scope.messageHistory[nick] === undefined) {
-			$scope.createPrivateTab(nick, false);
-		}
-		$scope.messageHistory[nick].push({
-			timestamp: Date.now(),
-			nick: nick,
-			message: message
-		});
-	});
-
-	// Sending the server that we want to join the room
-	socket.emit("joinroom", {room: $scope.currentRoom}, function(success, why){
-		if(success){
-			console.log("success");
-		} else {
-			console.log(":((");
-		}
-	});
-
-	// Operations for the page
-	$scope.backToRooms = function(){
-		socket.emit("partroom", $scope.currentRoom);
-		$state.go("rooms", { user: $scope.currentUser });
-	};
-
-	$scope.kickUser = function(userToKick){
-		socket.emit("kick", {user: userToKick, room: $scope.currentRoom}, function(success){
-			if(success){
-				console.log("kicked successfully");
-			} else {
-				console.log("kick failed");
+			else if(status === "quit"){
+				console.log("quit");
 			}
 		});
-	};
 
-	$scope.banUser = function(userToBan){
-		socket.emit("ban", {user: userToBan, room: $scope.currentRoom}, function(success){
-			if(success){
-				console.log("banned successfully");
-			} else {
-				console.log("ban failed");
+		socket.on("updateusers", function(roomName, users, ops){
+			if (roomName === $scope.currentRoom) {
+				$scope.currentUsers = users;
+				$scope.currentOps = ops;
 			}
+		});
+
+		socket.on("kicked", function(room, kickee, kicker){
+			if(kickee === $scope.currentUser){
+				$state.go("rooms", { user: $scope.currentUser });
+				toastr.error("Check yo self befo' yo wreck yo self!", "You've been kicked");
+			}
+		});
+
+		socket.on("banned", function(room, bannee, banner){
+			if(bannee === $scope.currentUser){
+				$state.go("rooms", { user: $scope.currentUser });
+				toastr.error("I guess you did not check yourself.", "You have been banned");
+			}
+		});
+
+		socket.on("recv_privatemsg", function(nick, message){
+			if ($scope.messageHistory[nick] === undefined) {
+				$scope.createPrivateTab(nick, false);
+			}
+			$scope.messageHistory[nick].push({
+				timestamp: Date.now(),
+				nick: nick,
+				message: message
+			});
+		});
+
+		// Sending the server that we want to join the room
+		socket.emit("joinroom", {room: $scope.currentRoom}, function(success, why){
+			if(success){
+				console.log("success");
+			} else {
+				console.log(":((");
+			}
+		});
+
+		// Operations for the page
+		$scope.backToRooms = function(){
+			socket.emit("partroom", $scope.currentRoom);
+			$state.go("rooms", { user: $scope.currentUser });
+		};
+
+		$scope.kickUser = function(userToKick){
+			socket.emit("kick", {user: userToKick, room: $scope.currentRoom}, function(success){
+				if(success){
+					console.log("kicked successfully");
+				} else {
+					console.log("kick failed");
+				}
+			});
+		};
+
+		$scope.banUser = function(userToBan){
+			socket.emit("ban", {user: userToBan, room: $scope.currentRoom}, function(success){
+				if(success){
+					console.log("banned successfully");
+				} else {
+					console.log("ban failed");
+				}
+			});
+		};
+
+		$scope.isUserOrOp = function(userToCheck){
+			if ($scope.currentOps[userToCheck] !== undefined ||
+				$scope.currentUser === userToCheck ||
+				$scope.currentOps[$scope.currentUser] === undefined){
+				return true;
+			}
+			return false;
+		};
+
+		$scope.privateMessage = function(user){
+			console.log("private message");
+			if ($scope.messageHistory[user] === undefined) {
+				$scope.createPrivateTab(user, true);
+			}
+		};
+
+		$scope.$on('$destroy', function() {
+			socket.getSocket().removeAllListeners();
 		});
 	};
 
-	$scope.isUserOrOp = function(userToCheck){
-		if ($scope.currentOps[userToCheck] !== undefined ||
-			$scope.currentUser === userToCheck ||
-			$scope.currentOps[$scope.currentUser] === undefined){
-			return true;
-		}
-		return false;
-	};
-
-	$scope.privateMessage = function(user){
-		console.log("private message");
-		if ($scope.messageHistory[user] === undefined) {
-			$scope.createPrivateTab(user, true);
-		}
-	};
-
-	$scope.$on('$destroy', function() {
-		socket.getSocket().removeAllListeners();
-	});
+	if ($rootScope.loggedIn) {
+		init();
+	}
+	else {
+		$state.go("login");
+	}
 }]);
--- src/js/controllers/RoomsController.js
+++ src/js/controllers/RoomsController.js
@@ -1,42 +1,52 @@
 ChatClient.controller("RoomsController",
-["$scope", "$state", "$stateParams", "socket",
-function ($scope, $state, $stateParams, socket){
-	$scope.rooms = [];
-	$scope.roomlist = {};
-	$scope.roomName = "";
-	$scope.currentUser = $stateParams.user;
-	$scope.errorMessage = "";
+["$scope", "$state", "$rootScope", "$stateParams", "socket",
+function ($scope, $state, $rootScope, $stateParams, socket){
+	var init = function() {
+		console.log("Initialize...");
+		$scope.rooms = [];
+		$scope.roomlist = {};
+		$scope.roomName = "";
+		$scope.currentUser = $stateParams.user;
+		$scope.errorMessage = "";
 
-	socket.on("roomlist", function(data) {
-		$scope.rooms = Object.keys(data);
-		$scope.roomlist = data;
-	});
+		socket.on("roomlist", function(data) {
+			$scope.rooms = Object.keys(data);
+			$scope.roomlist = data;
+		});
 
-	socket.emit("rooms");
+		$scope.createRoom = function(){
+			if($scope.roomName === "") {
+				$scope.errorMessage = "Please write a name for your room";
+				console.log("no room name");
+			} else {
+				socket.emit("joinroom", {room: $scope.roomName}, function(success, reason){
+					if(success){
+						console.log("success");
+						$state.go("room",
+							{ user: $scope.currentUser, room: $scope.roomName});
+						$scope.roomName = "";
+					} else {
+						console.log(reason);
+					}
+				});
+			}
+		};
 
-	$scope.createRoom = function(){
-		if($scope.roomName === "") {
-			$scope.errorMessage = "Please write a name for your room";
-			console.log("no room name");
-		} else {
-			socket.emit("joinroom", {room: $scope.roomName}, function(success, reason){
-				if(success){
-					console.log("success");
-					$state.go("room",
-						{ user: $scope.currentUser, room: $scope.roomName});
-					$scope.roomName = "";
-				} else {
-					console.log(reason);
-				}
-			});
-		}
+		$scope.notBanned = function(room){
+			var banlist = $scope.roomlist[room].banned;
+			if(banlist[$scope.currentUser] !== undefined){
+				return false;
+			}
+			return true;
+		};
+
+		socket.emit("rooms");
 	};
 
-	$scope.notBanned = function(room){
-		var banlist = $scope.roomlist[room].banned;
-		if(banlist[$scope.currentUser] !== undefined){
-			return false;
-		}
-		return true;
-	};
+	if ($rootScope.loggedIn) {
+		init();
+	}
+	else {
+		$state.go("login");
+	}
 }]);
